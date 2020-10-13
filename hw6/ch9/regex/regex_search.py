afbc405ae14ef32fbd90fe89d0029b9f6c1ea57f
*** Haley Harris *** <*** hbharris94@gmail.com ***>
2020-10-13 17:11:15 -0500

    added ch9 regex search project

---
+++ hw6/ch9/regex/regex_search.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python3
+''' Regex Search
+
+Author: Haley Harris
+'''
+import argparse, re, os
+from pathlib import Path
+
+def regex_search(folder_path, regex):
+    '''Given a path to a folder, opens all .txt files in a folder and
+    searches for any line that matches a user-supplied regular
+    expression. Returns all lines that contain the given regular
+    expression.
+
+    Args:
+      folder_path (str): Path to a folder in the file system
+      regex (str): Regular expression (as a string)
+
+    Returns:
+      list: List of all lines containing the regular expression
+
+    '''
+    
+    folder_path = Path(str(folder_path))
+    txt_files = list(folder_path.glob('*.txt'))
+
+    list_of_matches = []
+
+    for files in txt_files:
+
+       content = open(files, 'r')
+       lines = content.readlines()
+       
+       for line in lines:
+            search_txt_regex = re.compile(regex)
+            regex_match = re.findall(search_txt_regex, line)
+
+            if regex_match:
+                list_of_matches.append(line)
+
+    print(list_of_matches)
+
+
+folder = '/Users/haleyharris/Desktop/belhavencsc/csc221/hw6/ch9/regex/'
+regex1 = r'salamander'
+regex2 = r'kin*'
+
+# regex_search(folder, regex1)
+regex_search(folder, regex2)
+
+
+# def main():
+#     parser = argparse.ArgumentParser()
+#     parser.add_argument('folder', help='Folder to search for .txt files')
+#     parser.add_argument('regex', help='Regular expression to search for')
+
+#     args = parser.parse_args()
+
+#     for line in regex_search(args.folder, args.regex):
+#         print(line)
+
+# if __name__=='__main__':
+#     main()
+
+
